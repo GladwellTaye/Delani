170ecd42a18f65fa972b59c1bf7a3aafdbc2f846
GladwellTaye <gladwelltaye@gmail.com>
2020-02-24 15:04:48 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
-  $("#hide").click(function(){
-    $("p").hide();
-   });
-  
-  $(".col-md-4").click(function(){
-    $("#desIgn").toggle();
-    $("#hideP").toggle();
+  $("#development-image").click(function(){
+    $("#development-image").slideDown('1000').hide('800');
+    $("#development").show('1500');
   });
-})
+  $("#development").click(function(){
+    $("#development-image").slideDown('1100');
+    $("#development").slideUp('1000');
+  });
+  $("#design-image").click(function(){
+    $("#design-image").slideDown('1000').hide('1200');
+    $("#design").show('1000');
+  });
+  $("#design").click(function(){
+    $("#design").slideUp('1200');
+    $("#design-image").slideDown('1200');
+  });
+  $("#product-image").click(function(){
+    $("#product-image").slideDown('1500').hide('1000');
+    $("#product").show('1500');
+  });
+  $("#product").click(function(){
+    $("#product").slideUp('1500');
+    $("#product-image").slideDown('1500');
+  });
+});
+
