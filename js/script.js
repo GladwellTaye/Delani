050508f1ea3362b1218b248a3954df2b0b9d0f3d
GladwellTaye <gladwelltaye@gmail.com>
2020-02-25 07:38:39 +0300

    update

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   $("#development-image").click(function(){
     $("#development-image").slideDown('1000').hide('800');
     $("#development").show('1500');
+    $("#development p").show();
   });
   $("#development").click(function(){
     $("#development-image").slideDown('1100');
@@ -10,6 +11,7 @@ $(document).ready(function(){
   $("#design-image").click(function(){
     $("#design-image").slideDown('1000').hide('1200');
     $("#design").show('1000');
+    $("#design p").show();
   });
   $("#design").click(function(){
     $("#design").slideUp('1200');
